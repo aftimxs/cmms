f037fff87adc602befa2f694d6fa689649f2eb01
aftimxs <mario.aftimos@cetys.edu.mx>
2023-08-21 19:57:28 -0700

    left header

--- frontend/src/components/HeaderLeft.tsx
+++ frontend/src/components/HeaderLeft.tsx
@@ -1,4 +1,5 @@
 import ProgressBar from "./ProgressBar.tsx";
+import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
 
 interface Props {
     line: string;
@@ -14,77 +15,83 @@ const HeaderLeft = ( {line, pieces, scrap, total, current, previous}:Props ) => 
     let progress = [((pieces-scrap)/total)*100, (scrap/total)*100]
 
     return (
-        <div className="col-5 border border-danger">
+        <div className="col-5">
+            <div className="container py-0">
             <div className="row">
-                <div className="col-3">
+                <div className="col-4">
                     logo
                 </div>
-                <div className="col-7">
+                <div className="col-6">
                     <div className="row">
-                        PRODUCTION LINE
+                        <span className="titles">PRODUCTION LINE</span>
                     </div>
                     <div className="row">
-                        {line}
+                        <span>{line}</span>
                     </div>
                 </div>
                 <div className="col-2">
 
                 </div>
             </div>
-            <div className="row">
-                <div className="col-8">
-                    <div className="row">
-                        Shift Quantity
+            </div>
+
+            <div className="container py-0">
+                <div className="row">
+                    <div className="col-8">
+                        <div className="row">
+                            <span className="titles">SHIFT QUANTITY</span>
+                        </div>
+                        <div className="row">
+                            <span className="fs-2">{pieces} pcs</span>
+                        </div>
                     </div>
-                    <div className="row">
-                        {pieces} pcs
-                    </div>
-                </div>
-                <div className="col-4">
-                    <div className="row">
-                        OEE
-                    </div>
-                    <div className="row">
-                        65%
+                    <div className="col-4 text-center">
+                        <div className="row">
+                         <span className="titles">OEE</span>
+                        </div>
+                        <div className="row">
+                            <span className="fs-2">65%</span>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="row">
+            <div className="container py-0">
                 <div className="row">
-                    Current Batch
+                    <span className="titles">CURRENT BATCH</span>
                 </div>
                 <div className="row">
                     <div className="col-4">
-                        {current[0]}
+                        <span>{current[0]}</span>
                     </div>
                     <div className="col-8">
-                        {current[1]}
+                        <span>{current[1]}</span>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-7">
+                <div className="row pt-1">
+                    <div className="col-8 pt-1">
                         <ProgressBar progress={progress}/>
                     </div>
-                    <div className="col-5">
-                        {pieces} ({scrap}) / {total} pcs
+                    <div className="col-4 text-end">
+                        <span>{pieces} ({scrap}) / {total} pcs</span>
                     </div>
                 </div>
             </div>
-            <div className="row">
+
+            <div className="container py-0 pb-1">
                 <div className="row">
-                    <div className="col-4">
-                        Previous
+                    <div className="col-3 border-bottom border-2 border-success rounded text-center">
+                        <span className="titles">PREVIOUS</span>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-3">
-                        {previous[0]}
+                    <div className="col-4">
+                        <span>{previous[0]}</span>
                     </div>
-                    <div className="col-6">
-                        {previous[1]}
+                    <div className="col-4">
+                        <span>{previous[1]}</span>
                     </div>
-                    <div className="col-3">
-                        pieces
+                    <div className="col-4 text-end">
+                        <span>pcs</span>
                     </div>
                 </div>
             </div>

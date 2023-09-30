4868612cb043bd274119f5a4925660ab858a2f55
aftimxs <mario.aftimos@cetys.edu.mx>
2023-09-29 20:20:10 -0700

    fix

--- frontend/src/components/Timeline/CommentModal.tsx
+++ frontend/src/components/Timeline/CommentModal.tsx
@@ -4,7 +4,7 @@ import {Container, InputAdornment, TextField} from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 import dayjs from "dayjs";
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -14,15 +14,23 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
-import {NavigateBeforeOutlined, NavigateNextOutlined, Circle,
-    ExpandLess, ExpandMore, EditCalendar, Engineering, AltRoute}
-    from '@mui/icons-material';
+import {
+    AltRoute,
+    Circle,
+    EditCalendar,
+    Engineering,
+    ExpandLess,
+    ExpandMore,
+    NavigateBeforeOutlined,
+    NavigateNextOutlined
+} from '@mui/icons-material';
 import {useEffect, useState} from "react";
 import _ from 'lodash';
 import {produce} from "immer"
 import {
     useDowntimeUpdatedMutation,
-    useLazyGetDowntimeQuery, useLazyGetScrapQuery,
+    useGetDowntimeQuery,
+    useGetScrapQuery,
     useScrapAddedMutation,
     useScrapDeletedMutation,
     useScrapUpdatedMutation
@@ -30,7 +38,7 @@ import {
 import CommentButton from "./CommentButton.tsx";
 import {useAppSelector} from "../../app/hooks.ts";
 import ScrapButton from "./ScrapButton.tsx";
-
+import {skipToken} from "@reduxjs/toolkit/query";
 
 
 const style = {
@@ -103,20 +111,24 @@ const CommentModal = ({ open, setOpen, handleClick, setBarReason }:any) => {
     //GET SELECTED BAR INFO
     const bar = useAppSelector(state => state.downtime)
 
-    //LAZY QUERIES TRIGGERS DECLARATION
-    const [getDowntime, {data:comments}] = useLazyGetDowntimeQuery()
-    const [getScrap, {data:scrap}] = useLazyGetScrapQuery()
+    ////LAZY QUERIES TRIGGERS DECLARATION
+    //const [getDowntime, {data:comments}] = useLazyGetDowntimeQuery()
+    //const [getScrap, {data:scrap}] = useLazyGetScrapQuery()
+//
+    ////PULL INFO DEPENDING ON BAR TYPE
+    //useEffect(() => {
+    //    if (bar.background === 'bg-success' ||  bar.background === 'bg-warning') {
+    //        getScrap({id: `S${dayjs(bar.start, 'DD-MM-YYYY HH:mm:ss Z').format('DDMMYYHHmm')}${bar.shift}`})
+    //    } else if (bar.background === 'bg-danger'){
+    //        getDowntime(bar)
+    //    }
+    //}, [bar]);
 
-    //PULL INFO DEPENDING ON BAR TYPE
-    useEffect(() => {
-        if (bar.background === 'bg-success' ||  bar.background === 'bg-warning') {
-            getScrap({id: `S${dayjs(bar.start, 'DD-MM-YYYY HH:mm:ss Z').format('DDMMYYHHmm')}${bar.shift}`})
-        } else if (bar.background === 'bg-danger'){
-            getDowntime(bar)
-        }
-    }, [bar]);
+    const {data:scrap} = useGetScrapQuery(bar.background !== 'bg-danger' ?
+        {id: `S${dayjs(bar.start, 'DD-MM-YYYY HH:mm:ss Z').format('DDMMYYHHmm')}${bar.shift}`} : skipToken);
 
-    console.log(scrap)
+    const {data:comments} = useGetDowntimeQuery(bar.background === 'bg-danger' ? bar : skipToken);
+
 
     //LOGIC TO CHECK IF THERE IS INFO IN THE QUERIED VARIABLES
     let start = dayjs(bar.start, 'DD-MM-YYYY HH:mm:ss Z').format('h:mm a');
@@ -366,7 +378,6 @@ const CommentModal = ({ open, setOpen, handleClick, setBarReason }:any) => {
                                         sx={{mt:'10px'}}
                                         defaultValue={`${scrapQuantity}`}
                                         onClick={() => {
-                                            if (scrap === undefined) {
                                                 postScrap({
                                                     id: `S${bar.id}`,
                                                     reason: null,
@@ -375,13 +386,10 @@ const CommentModal = ({ open, setOpen, handleClick, setBarReason }:any) => {
                                                     minute: dayjs(bar.start, 'DD-MM-YYYY HH:mm:ss Z').format('HH:mm:ss'),
                                                     shift: bar.shift,
                                                 })
-                                            }
                                         }}
                                         onChange={(event) => updateScrap({
                                             id: `S${bar.id}`,
-                                            reason: null,
                                             pieces: event.target.value,
-                                            comments: null,
                                             minute: dayjs(bar.start, 'DD-MM-YYYY HH:mm:ss Z').format('HH:mm:ss'),
                                             shift: bar.shift
                                         })}
--- frontend/src/components/Timeline/ScrapIndicator.tsx
+++ frontend/src/components/Timeline/ScrapIndicator.tsx
@@ -1,19 +1,12 @@
 import Box from "@mui/material/Box";
-import {useEffect, useState} from "react";
 import {useGetScrapQuery} from "../../app/services/apiSplice.ts";
-import _ from "lodash";
 import {skipToken} from "@reduxjs/toolkit/query";
 
 const ScrapIndicator = ({queryScrap, id}) => {
 
     const {data:scrap} = useGetScrapQuery(queryScrap ? {id:'S'+id} : skipToken);
 
-    //const color = scrap ? '#ffc107' : '#FFFFFF';
-    const [color, setColor] = useState('#FFFFFF')
-
-    useEffect(() => {
-        setColor(scrap ? '#ffc107' : '#FFFFFF')
-    }, [scrap]);
+    const color = scrap ? scrap.pieces ?  '#ffc107' : '#FFFFFF' : '#FFFFFF';
 
     return(
         <Box sx={{
